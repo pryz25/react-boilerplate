4806e914870e7d0912107303598504fe37aa8c2f
Peter R <pryzanych@gmail.com>
2020-08-28 18:12:38 -0700

    Put header back in. Alter button to different page

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -14,6 +14,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Header from 'components/Header';
 
 import GlobalStyle from '../../global-styles';
 
@@ -35,7 +36,7 @@ export default function App() {
       >
         <meta name="description" content="A React.js Boilerplate application" />
       </Helmet>
-      {/* <Header /> */}
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/features" component={FeaturePage} />
