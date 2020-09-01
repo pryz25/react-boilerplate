c4daa5b77d834cf879f0f28ec76f4fae281c8c1c
Peter R <pryzanych@gmail.com>
2020-09-01 12:43:39 -0700

    Refactor name on githubData. Use takeEvery so all data is loaded each call

--- app/containers/HomePage/saga.js
+++ app/containers/HomePage/saga.js
@@ -2,7 +2,7 @@
  * Gets the repositories of the user from Github
  */
 
-import { call, put, select, takeLatest } from 'redux-saga/effects';
+import { call, put, select, takeEvery } from 'redux-saga/effects';
 import { LOAD_STRINGS } from 'containers/App/constants';
 import { reposLoaded, repoLoadingError } from 'containers/App/actions';
 
@@ -29,10 +29,10 @@ export function* getStrings() {
 /**
  * Root saga manages watcher lifecycle
  */
-export default function* githubData() {
+export default function* stringsData() {
   // Watches for LOAD_STRINGS actions and calls getStrings when one comes in.
   // By using `takeLatest` only the result of the latest API call is applied.
   // It returns task descriptor (just like fork) so we can continue execution
   // It will be cancelled automatically on component unmount
-  yield takeLatest(LOAD_STRINGS, getStrings);
+  yield takeEvery(LOAD_STRINGS, getStrings);
 }
