8b1b41175468b5ea8fcda200b6c3350d0e89a667
Peter R <pryzanych@gmail.com>
2020-09-02 13:31:03 -0700

    Refactor to try and fix Store connections

--- app/containers/HomePage/saga.js
+++ app/containers/HomePage/saga.js
@@ -2,7 +2,7 @@
  * Gets the repositories of the user from Github
  */
 
-import { call, put, takeEvery } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import { LOAD_STRINGS } from 'containers/App/constants';
 import { reposLoaded, repoLoadingError } from 'containers/App/actions';
 
@@ -31,5 +31,5 @@ export default function* stringsData() {
   // By using `takeLatest` only the result of the latest API call is applied.
   // It returns task descriptor (just like fork) so we can continue execution
   // It will be cancelled automatically on component unmount
-  yield takeEvery(LOAD_STRINGS, getStrings);
+  yield takeLatest(LOAD_STRINGS, getStrings);
 }
