52673b60ba90457433380ec1ccb04e119a0f7ef6
Peter R <pryzanych@gmail.com>
2020-09-01 20:40:02 -0700

    Create functions to use Redux Store for form values

--- app/containers/FeaturePage/index.js
+++ app/containers/FeaturePage/index.js
@@ -6,14 +6,42 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
 
+import { useInjectReducer } from 'utils/injectReducer';
+import { useInjectSaga } from 'utils/injectSaga';
+import request from 'utils/request';
 import H1 from 'components/H1';
 import Section from './Section';
 import Form from './Form';
 import Input from './Input';
 import messages from './messages';
+import reducer from './reducer';
+import saga from './saga';
 
-export default function FeaturePage() {
+const key = 'feature';
+
+export function FeaturePage({ newString, changeString}) {
+  useInjectReducer({ key, reducer });
+  useInjectSaga({ key, saga });
+  const url = `http://localhost:3000/api/strings`;
+
+  function onSubmitForm(event) {
+    if (event) event.preventDefault();
+    console.log(newString);
+    return request(url, {
+      method: 'POST',
+      body: newString,
+    }).then(response => response);
+  }
+
+  function onStringChange(event) {
+    if (event) event.preventDefault();
+  }
+
   return (
     <div>
       <Helmet>
@@ -29,17 +57,36 @@ export default function FeaturePage() {
       <Section>
         <Form onSubmit={onSubmitForm}>
           <label htmlFor="newString">
-            <span>Enter a new string for the list here</span>
-            <Input
-              id="newString"
-              type="text"
-              placeholder="mxstbr"
-              value={newString}
-              onChange={onChangeString}
-            />
+            <span>Enter a new string for the list here:</span>
+            <Input id="newString" type="text" onChange={onStringChange} />
           </label>
         </Form>
       </Section>
     </div>
   );
 }
+
+FeaturePage.propTypes = {
+  newString: PropTypes.string,
+  onStringChange: PropTypes.func,
+};
+
+const mapStateToProps = createStructuredSelector({
+  newString: makeSelectString(),
+});
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onStringChange: event => dispatch(changeString(event.target.value)),
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(FeaturePage);
